952c3a774f72b6517f26eda8543b2c3a902110cb
lorintrandafir <lorintrandafir@yahoo.com>
2026-09-18 16:29:59 +0200

    Fix Java 17 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
 android {
     namespace = "com.lorin.noatranslator"
     compileSdk = 35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
     defaultConfig {
         applicationId = "com.lorin.noatranslator"
